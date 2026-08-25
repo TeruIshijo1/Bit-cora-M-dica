4e5b2d8d0b89c9c9401ab321d757bd064a0a934d
Alberto García Mendoza <alberto.garmdz@gmail.com>
2026-08-24 18:35:14 -0600

    fix: restaurar compatibilidad de iniciar.bat con entorno de produccion

--- iniciar.bat
+++ iniciar.bat
@@ -9,8 +9,8 @@ echo - http://192.168.254.150:8000 (Desde cualquier otra PC o Tablet en la red)
 echo ===================================================
 
 echo ===================================================
-echo Iniciando el servidor principal de Python y el servicio Biometrico...
-npx concurrently -n "PYTHON,BIOMETRICO" -c "bgBlue.bold,bgMagenta.bold" "cd backend && (if exist venv\Scripts\activate.bat call venv\Scripts\activate.bat) && python seed.py && python -m uvicorn main:app --host 0.0.0.0 --port 8000" "cd ../Teru/Bio-security && node server.js"
+echo Iniciando el servidor principal de Python y "Bio-security"...
+npx concurrently -n "PYTHON,BIO-SECURITY" -c "bgBlue.bold,bgMagenta.bold" "cd backend && (if exist venv\Scripts\activate.bat call venv\Scripts\activate.bat) && python seed.py && python -m uvicorn main:app --host 0.0.0.0 --port 8000" "IF EXIST backend_node\server.js (cd backend_node && node server.js) ELSE (cd ../Teru/Bio-security && node server.js)"
 
 :: El script termina aquí.
 exit
